9bd0b397c3846fed27c788912c5797e3e8b40a40
Lamas Neil <neillamas@gmail.com>
2022-09-11 17:31:54 +0200

    first commit

---
+++ dao_pymongo/meta_functions.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def is_compound(object):
+    if (
+        isinstance(object, list)
+        or isinstance(object, tuple)
+        or isinstance(object, dict)
+        or isinstance(object, np.ndarray)
+    ):
+        return True
+    else:
+        return False
+
+
+def get_values_from_compound(object):
+    return list(object.values()) if isinstance(object, dict) else object
